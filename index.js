da31693faa8ed71f9ac73a3579ae2d8b97108e2a
slaapz <slaapz@gmail.com>
2018-02-12 18:39:00 -0700

    Added alert feature

--- index.js
+++ index.js
@@ -3,7 +3,35 @@ const searchInput = document.getElementById('search-input');
 
 
 searchForm.addEventListener('submit', e => {
-  console.log(searchInput.value);
+  // Get the search term
+  const searchTerm = searchInput.value;
+  // Get sort
+  const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+  // Get limit
+  const searchLimit = document.getElementById('limit').value;
+  console.log(searchLimit);
+
+  // Check input
+  if(searchTerm === ''){
+    showMessage('Please add a search term', 'alert-danger');
+  }
+  
+  function showMessage(msg, className){
+    // create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    //Add text
+    div.appendChild(document.createTextNode(msg));
+    //Get parent
+    const searchContainer = document.getElementById('search-container');
+    // Get search
+    const search = document.getElementById('search');
+    searchContainer.insertBefore(div, search);
+
+    //Time out alert 
+    setTimeout(() => document.querySelector('.alert').remove(),3000);
+  }
 
 
   e.preventDefault();
